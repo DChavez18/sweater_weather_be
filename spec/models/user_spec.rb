7d7f34ee72c1c9d6b111741c663d635e692c5012
Derek Chavez <frrenose17@yahoo.com>
2023-09-24 10:35:44 -0600

    test: add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_confirmation_of :password }
+  end
+
+  describe "class methods" do
+    describe ".generate_api_key" do
+      it "generates an api key for a user" do
+        user = User.create!(email: "test@test.com", password: "password", password_confirmation: "password")
+
+        expect(user.api_key).to_not be_nil
+      end
+    end
+  end
 end
