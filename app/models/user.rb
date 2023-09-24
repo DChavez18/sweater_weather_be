f08a136520ab1e1a7cd880aad829e6e26c7373db
Derek Chavez <frrenose17@yahoo.com>
2023-09-24 09:53:50 -0600

    feat: add users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true, confirmantion: true, length: {minimum: 6}
+  validates :password, presence: true, confirmation: true, length: {minimum: 6}
   has_secure_password
 
   before_create :generate_api_key
--- spec/requests/api/v0/create_a_user_spec.rb
+++ spec/requests/api/v0/create_a_user_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "User Registration" do
   describe "happy path" do
     it "creates a user and returns an api key" do
-      post "/api/v1/users", params: {email: test_email, password: test_password, password_confirmation: test_password}, as: :json
+      post "/api/v0/users", params: {email: "test_email@test.com", password: "password", password_confirmation: "password"}, as: :json
       headers = {"CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"}
 
       expect(response).to be_successful
